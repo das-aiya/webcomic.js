d0b5fef98163cf1d1710fb2a21913b61a2982d86
Rick Dominguez <sketchinsketcher@hotmail.com>
2015-04-04 05:16:40 -0400

    see README

--- routes/rss.js
+++ routes/rss.js
@@ -7,12 +7,13 @@ var xlmns = {
 		'xmlns:content': "http://purl.org/rss/1.0/modules/content/" ,
 		// 'xmlns:wfw': "http://wellformedweb.org/CommentAPI/",
 		// 'xmlns:dc': "http://purl.org/dc/elements/1.1/",
-		// 'xmlns:atom': "http://www.w3.org/2005/Atom",
+		'xmlns:atom': "http://www.w3.org/2005/Atom",
 		// 'xmlns:sy': "http://purl.org/rss/1.0/modules/syndication/",
-		// 'xmlns:slash': "http://purl.org/rss/1.0/modules/slash/"
+		'xmlns:slash': "http://purl.org/rss/1.0/modules/slash/"
 }
 
 renderRSS = function (req, res, next) {
+	res.header("Content-Type", "application/rss+xml")
 	useful.buildUpdates(20, function (err, results) {
 		if (err) {
 			res.send(err)
