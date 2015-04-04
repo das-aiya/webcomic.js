31d6710c93f3a0f479851d475de2546bf7a3a437
Rick Dominguez <sketchinsketcher@hotmail.com>
2015-04-04 05:07:18 -0400

    updated rss

--- routes/rss.js
+++ routes/rss.js
@@ -1,12 +1,15 @@
 var useful = require('../lib/useful.js')
+var config = require('../package')
+var path = require('path')
+var moment = require('moment')
 
 var xlmns = {
 		'xmlns:content': "http://purl.org/rss/1.0/modules/content/" ,
-		'xmlns:wfw': "http://wellformedweb.org/CommentAPI/",
-		'xmlns:dc': "http://purl.org/dc/elements/1.1/",
-		'xmlns:atom': "http://www.w3.org/2005/Atom",
-		'xmlns:sy': "http://purl.org/rss/1.0/modules/syndication/",
-		'xmlns:slash': "http://purl.org/rss/1.0/modules/slash/"
+		// 'xmlns:wfw': "http://wellformedweb.org/CommentAPI/",
+		// 'xmlns:dc': "http://purl.org/dc/elements/1.1/",
+		// 'xmlns:atom': "http://www.w3.org/2005/Atom",
+		// 'xmlns:sy': "http://purl.org/rss/1.0/modules/syndication/",
+		// 'xmlns:slash': "http://purl.org/rss/1.0/modules/slash/"
 }
 
 renderRSS = function (req, res, next) {
@@ -15,7 +18,9 @@ renderRSS = function (req, res, next) {
 			res.send(err)
 		} else {
 			results.xlmns = xlmns
-			console.log(results)
+			results.config = config
+			results.path = path
+			results.moment = moment
 			res.render('rss', results)
 		}
 	})
